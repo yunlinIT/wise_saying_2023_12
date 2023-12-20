bfe56176c442063a6a2e656434d963f5f740e51d
yunlinIT <yunlinit@gmail.com>
2023-12-20 17:54:48 +0900

    5단계, 목록 시에 정보가 제대로 나오도록 수정

--- src/App.java
+++ src/App.java
@@ -32,14 +32,21 @@ public class App {
 				wiseSayings.add(wiseSaying);
 
 				System.out.printf("%d번 명언이 등록되었습니다.\n", id);
-				lastId++;// 마지막 번호 증가
+				lastId++;
 
 			} else if (cmd.equals("목록")) {
 				if (wiseSayings.size() == 0) {
 					System.out.println("등록 된 명언이 없어");
 				} else {
-					System.out.println("있던데???");
-					System.out.println("등록 된 명언 수 : " + wiseSayings.size());
+					System.out.println("번호  /  작가  /  명언  ");
+					System.out.println("=".repeat(30));
+
+					for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+						WiseSaying ws = wiseSayings.get(i);
+
+						System.out.printf("%d  /  %s  /  %s\n", ws.getId(), ws.getAuthor(), ws.getContent());
+					}
+
 				}
 			} else {
 				System.out.println("존재하지 않는 명령어입니다");
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); 
 
 		new App(sc).run();
 
