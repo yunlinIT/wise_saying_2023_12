41339b302f19728afd740a2d9a4517310c1e1fc2
yunlinIT <yunlinit@gmail.com>
2023-12-20 11:54:33 +0900

    1단계, 종료기능구현

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+
+		new App(sc).run();
+
+		sc.close();
 	}
-
 }
