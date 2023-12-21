5894c4f1b21a82cad64a835859f2709c2f000fbc
yunlinIT <yunlinit@gmail.com>
2023-12-21 15:19:44 +0900

    10단계, Rq 클래스 도입, 명령어 분석 기능 요청

--- src/com/ws/App.java
+++ src/com/ws/App.java
@@ -31,29 +31,14 @@ public class App {
 			} else if (cmd.equals("목록")) {
 				wiseSayingController.list();
 			} else if (cmd.startsWith("삭제")) {
-				// parsing
 
-				String[] cmdBits = cmd.split("\\?", 2);
+				Rq rq = new Rq(cmd);
 
-				String actionCode = cmdBits[0];
-				Map<String, String> params = new HashMap<>();
+				System.out.println("actionCode : " + rq.getActionCode());
+				System.out.println("params.id : " + rq.getParam("id"));
+				System.out.println("params.author : " + rq.getParam("author"));
+				System.out.println("params.content : " + rq.getParam("content"));
 
-				String[] paramBits = cmdBits[1].split("&");
-
-				for (String paramStr : paramBits) {
-					String[] paramStrBits = paramStr.split("=", 2);
-					String key = paramStrBits[0];
-					String value = paramStrBits[1];
-					System.out.println("key : " + key);
-					System.out.println("value : " + value);
-					params.put(key, value);
-				}
-
-				System.out.println(Arrays.toString(cmdBits));
-				System.out.println("actionCode : " + actionCode);
-				System.out.println("params : " + params);
-				// parsing
-				
 				wiseSayingController.remove();
 			} else {
 				System.out.println("존재하지 않는 명령어입니다");
---
+++ src/com/ws/Rq.java
@@ -0,0 +1,36 @@
+package com.ws;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// Rq == Request(요청)
+public class Rq {
+	private String actionCode;
+	private Map<String, String> params;
+
+	public Rq(String cmd) {
+		String[] cmdBits = cmd.split("\\?", 2);
+
+		actionCode = cmdBits[0];
+
+		params = new HashMap<>();
+
+		String[] paramBits = cmdBits[1].split("&");
+
+		for (String paramStr : paramBits) {
+			String[] paramStrBits = paramStr.split("=", 2);
+			String key = paramStrBits[0];
+			String value = paramStrBits[1];
+			params.put(key, value);
+		}
+	}
+
+	public String getActionCode() {
+		return actionCode;
+	}
+
+	public String getParam(String name) {
+		return params.get(name);
+	}
+
+}
